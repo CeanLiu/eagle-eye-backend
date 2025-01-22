e636b8b95b929e537d25da53ba6f8e4d19960048
Cean <142998033+CeanLiu@users.noreply.github.com>
2025-01-21 21:28:07 -0500

    updated server, don't need websocket

--- server.js
+++ server.js
@@ -2,34 +2,26 @@ import "dotenv/config";
 import express from "express";
 import { errorHandler } from "./src/utils/middleware.js";
 import esp32Router from "./src/routes/esp32.js";
-import { WebSocketServer } from "ws";
 
 const app = express();
 
-// Create the HTTP server using Express
-const server = app.listen(process.env.PORT, process.env.PUBLIC_IP, () => {
+// Middleware to parse JSON
+app.use(express.json());
+
+// Start the HTTP server using Express
+app.listen(process.env.PORT, process.env.PUBLIC_IP, () => {
   console.log(
     `Server running on http://${process.env.PUBLIC_IP}:${process.env.PORT}`
   );
 });
 
-const wss = new WebSocketServer({ server });
-
-wss.on("connection", (ws) => {
-  console.log("A new WebSocket connection established.");
-
-  ws.send(JSON.stringify({ message: "Welcome to the WebSocket server!" }));
-  ws.on("message", (message) => {
-    console.log("Received message:", message);
-    ws.send(`Echo: ${message}`);
-  });
-  ws.on("close", () => {
-    console.log("A WebSocket connection was closed.");
-  });
-});
-
 // Use the esp32 routes
 app.use("/esp32", esp32Router);
+
+// Example route for handling GET requests
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the Express server!" });
+});
 
 // Error handling middleware
 app.use(errorHandler);
--- src/routes/esp32.js
+++ src/routes/esp32.js
@@ -4,5 +4,8 @@ import { checkEsp32 } from "../utils/middleware.js";
 const router = express.Router();
 
 router.post("/stream", checkEsp32, esp32Controller.streamEsp32);
+router.get("/", (req, res) => {
+  res.send("FUCK YOU");
+});
 
 export default router;
