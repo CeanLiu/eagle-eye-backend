7c40ab7a3a784bf73376a884b8ec91bcc9b19864
Cean <142998033+CeanLiu@users.noreply.github.com>
2025-01-27 18:32:34 -0500

    changed constants to environmental variables

--- src/controllers/esp32.js
+++ src/controllers/esp32.js
@@ -1,13 +1,12 @@
+import "dotenv/config";
 import {
   wasRecording,
   isRecording,
   resetIsRecording,
   capture,
   resetCapture,
-  isLightOn,
 } from "./user.js";
 import * as fileUtils from "../utils/files.js";
-import path from "path";
 
 export async function handleImage(clients, image) {
   for (const client of clients) {
@@ -18,22 +17,21 @@ export async function handleImage(clients, image) {
 
   //handle recording(process frames and download to local)
   if (isRecording && !wasRecording) {
-    const outputDir = "public/assets/temp";
-    await fileUtils.saveFrame(outputDir, image, false);
+    await fileUtils.saveFrame(process.env.TEMP_FRAME_PATH, image, false);
   } else if (!isRecording && wasRecording) {
     resetIsRecording();
-    const outputDir = "public/assets/videos";
-    const inputDir = "public/assets/temp";
     fileUtils.resetFC();
-    await fileUtils.processVideo(inputDir, outputDir);
+    await fileUtils.processVideo(
+      process.env.TEMP_FRAME_PATH,
+      process.env.VIDEO_STORAGE_PATH
+    );
     await fileUtils.deleteDir(inputDir);
     await fileUtils.makeDir(inputDir);
   }
 
   //capture frame and download to local
   if (capture) {
-    const outputDir = "public/assets/photos";
-    await fileUtils.saveFrame(outputDir, image, true);
+    await fileUtils.saveFrame(process.env.CAPTURE_SORAGE_PATH, image, true);
     resetCapture();
   }
 }
@@ -45,5 +43,3 @@ export function handleMessage(clients, message) {
     }
   }
 }
-
-export function sendMessage() {}
